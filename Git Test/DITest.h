f4262f8a601acdfc5cf7c9c2cba55328849e3a35
Todd Teta <toddteta@Todds-MacBook-Pro-2.local>
2014-04-10 23:40:25 -0700

    added a test class

---
+++ Git Test/DITest.h
@@ -0,0 +1,13 @@
+//
+//  DITest.h
+//  Git Test
+//
+//  Created by Todd Teta on 4/10/14.
+//  Copyright (c) 2014 Dynamite Investments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DITest : NSObject
+
+@end
---
+++ Git Test/DITest.m
@@ -0,0 +1,13 @@
+//
+//  DITest.m
+//  Git Test
+//
+//  Created by Todd Teta on 4/10/14.
+//  Copyright (c) 2014 Dynamite Investments. All rights reserved.
+//
+
+#import "DITest.h"
+
+@implementation DITest
+
+@end
